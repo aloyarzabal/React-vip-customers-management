9c063e952586f1f479c43156814448d2f25c563b
Adolfo Lopez <adolfo.lopez@infor.com>
2022-09-06 17:15:44 +0200

    Add css modification
    find customer added reduce hook + visual form

--- src/Components/NewCustomer/NewCustomer.js
+++ src/Components/NewCustomer/NewCustomer.js
@@ -1,19 +1,57 @@
-import { useContext, useRef, useState } from 'react';
+import { useContext, useReducer, useRef, useState } from 'react';
 import Modal from '../UI/Modal';
 import classes from "../Search/SearchMenu.module.css";
 import { MainTaskContext } from "../../App";
 
+
+const reducerFunction = (state, action) => {
+    if (action.type === "INVALID_PHONE") {
+        return {
+            validCustomer: false,
+            validNewPhone: false,
+
+            customerCreated: false,
+            validNewEmail: true,
+            randomNewCustomerNumber: ''
+        }
+    }
+    if (action.type === "INVALID_EMAIL") {
+        return {
+            validCustomer: false,
+            validNewEmail: false,
+
+            customerCreated: false,
+            validNewPhone: true,
+            randomNewCustomerNumber: ''
+
+        }
+    }
+    if (action.type === "VALID_FORM") {
+        return {
+            customerCreated: true,
+            validCustomer: true,
+            validNewEmail: true,
+            validNewPhone: true,
+            randomNewCustomerNumber: state.randomNewCustomerNumber
+        }
+    }
+}
+
 const NewCustomer = (props) => {
 
+    const [state, dispatch] = useReducer(reducerFunction, {
+        validCustomer: true,
+        customerCreated: false,
+        validNewPhone: true,
+        validNewEmail: true,
+        randomNewCustomerNumber: ''
+    });
     const [validForm, setValidForm] = useState(true);
-    const [validCustomer, setValidCustomer] = useState(true);
-    const [validNewEmail, setValidNewEmail] = useState(true);
-    const [validNewPhone, setValidNewPhone] = useState(true);
-    const [customerCreated, setCustomerCreated] = useState(false);
-    const [validEmail, setValidEmail] = useState(true);
-    const [validPhone, setValidPhone] = useState(true);
+    const [validEmailFormat, setValidEmailFormat] = useState(true);
+    const [validPhoneFormat, setValidPhoneFormat] = useState(true);
 
-    const { DUMMY_CUSTOMERS, setCustomerList } = useContext(MainTaskContext);
+
+    const { DUMMY_CUSTOMERS } = useContext(MainTaskContext);
 
     const surnameRef = useRef(null);
     const nameRef = useRef(null);
@@ -34,7 +72,7 @@ const NewCustomer = (props) => {
         const introducedEmail = emailRef.current.value;
         const introducedDate = dateRef.current.value;
         const introducedGender = ((maleRef.current.checked) ? 1 : 2);
-        
+
 
         if (!introducedSurname || !introduceName || !introducedPhone || !introducedEmail || !introducedDate) {
             nameRef.current.focus();
@@ -43,27 +81,30 @@ const NewCustomer = (props) => {
         else {
             setValidForm(true);
 
-            (introducedPhone.length !== 9) ? setValidPhone(false) : setValidPhone(true);
-            (!introducedEmail.includes('@')) ? setValidEmail(false) : setValidEmail(true);
+            (introducedPhone.length !== 9) ? setValidPhoneFormat(false) : setValidPhoneFormat(true);
+            (!introducedEmail.includes('@')) ? setValidEmailFormat(false) : setValidEmailFormat(true);
 
             if (introducedPhone.length === 9 && introducedEmail.includes('@')) {
 
-                let filteredCustomerList = [...DUMMY_CUSTOMERS];                
+                let filteredCustomerList = [...DUMMY_CUSTOMERS];
 
                 if (filteredCustomerList.filter((a) => a.phone.includes(introducedPhone)).length > 0) {
-                    setValidCustomer(false);
-                    setValidNewPhone(false);
+                    dispatch({ type: "INVALID_PHONE" });
                 }
-
-                else if (filteredCustomerList = filteredCustomerList.filter((a) => a.email.includes(introducedEmail)).length > 0) {
-                    setValidCustomer(false);
-                    setValidNewEmail(false);
+                else if (filteredCustomerList.filter((a) => a.email.includes(introducedEmail)).length > 0) {
+                    dispatch({ type: "INVALID_EMAIL" });
                 }
 
                 else {
 
+                    //create a random number to be associated to the new customer                    
+                    do {
+                        state.randomNewCustomerNumber = Math.floor((Math.random() * 99999) + 1);
+
+                    } while (filteredCustomerList.filter((a) => String(a.customer_number).includes(state.randomNewCustomerNumber)).length > 0);
+
                     const customerToAdd = {
-                        customer_number: 123123,
+                        customer_number: state.randomNewCustomerNumber,
                         name: introduceName,
                         surname: introducedSurname,
                         second_surname: introducedSecondSurname,
@@ -85,10 +126,7 @@ const NewCustomer = (props) => {
                     emailRef.current.value = '';
                     dateRef.current.value = '';
 
-                    setCustomerCreated(true);
-                    setValidCustomer(true);
-                    setValidNewEmail(true);
-                    setValidNewPhone(true);
+                    dispatch({ type: "VALID_FORM" });
                 }
             }
 
@@ -96,6 +134,8 @@ const NewCustomer = (props) => {
     }
 
     const formClasses = `${classes.form} ${validForm ? '' : classes.incorrectData}`;
+    const phoneClasses = `${(validPhoneFormat && state.validNewPhone) ? '' : classes.incorrectField}`;
+    const emailClasses = `${(validEmailFormat && state.validNewEmail) ? '' : classes.incorrectField}`;
 
     return <Modal onClose={props.onClose}>
         <form className={formClasses}>
@@ -105,27 +145,29 @@ const NewCustomer = (props) => {
             <label>Name</label>
             <input type="text" ref={surnameRef} required></input>
             <label>Surname</label>
-            <input type="text" id='secondSurname' ref={secondSurnameRef} placeholder='(Optional)'></input>
+            <input type="text" id='secondSurname' ref={secondSurnameRef} required></input>
             <label>Second Surname</label>
-            <input type="text" ref={phoneRef} required></input>
+            <input type="text" ref={phoneRef} className={phoneClasses} required></input>
             <label>Phone</label>
-            <input type="type" ref={emailRef} required></input>
+            <input type="type" ref={emailRef} className={emailClasses} required></input>
             <label>E-mail</label>
             <input style={{ textAlign: 'right' }} type="date" ref={dateRef} required></input>
             <label>Date of birth</label>
-            <input type="radio" id="male" value='male' name="gender" checked ref={maleRef}></input>
-            <label for="male">male</label>
-            <input type="radio" id="female" value='female' name="gender" ref={femaleRef}></input>
-            <label for="female">female</label>
+            <div className={classes.genderBlock}>
+                <input type="radio" id="male" value='male' name="gender" defaultChecked ref={maleRef}></input>
+                <label htmlFor="male">male</label>
+                <input type="radio" id="female" value='female' name="gender" ref={femaleRef}></input>
+                <label htmlFor="female">female</label>
+            </div>
+            <label>Gender:</label>
 
             {!validForm && <p>Please fulfill all the mandatory fields!</p>}
-            {!validEmail && <p>Introduce a valid email format "test@test.com"</p>}
-            {!validPhone && <p>The phone number should have 9 digits</p>}
-            {!validCustomer && <p>The customer already exists in the database.</p>}
-            {!validNewEmail && <p>Please try another email.</p>}
-            {!validNewPhone && <p>Please try another phone.</p>}
-
-            {customerCreated && <p>Customer created succesfully!</p>}
+            {!validEmailFormat && <p>Introduce a valid email format "test@test.com"</p>}
+            {!validPhoneFormat && <p>The phone number should have 9 digits</p>}
+            {!state.validCustomer && <p>The customer already exists in the database.</p>}
+            {!state.validNewEmail && <p>Please try another email.</p>}
+            {!state.validNewPhone && <p>Please try another phone.</p>}
+            {state.customerCreated && <p>Customer created succesfully with number: {state.randomNewCustomerNumber}!</p>}
 
             <div className={classes.actions}>
                 <button onClick={createCustomerHandler} className={classes.find}> Create</button>
